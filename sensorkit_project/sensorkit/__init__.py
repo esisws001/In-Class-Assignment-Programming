3ee49f78267f1808788b70e67421fe41d60aba9b
hvd4x <hudasuleman12@gmail.com>
2026-08-05 16:57:28 +0000

    __init__.py

--- sensorkit_project/sensorkit/__init__.py
+++ sensorkit_project/sensorkit/__init__.py
@@ -9,5 +9,15 @@ It exposes the public API so a user can simply write:
 
 If you add a StrainGauge class in sensors.py, add it to the import below too.
 """
-#example from .base import Sensor
 
+from .base import Sensor
+from .sensors import Thermocouple  # Add StrainGauge here if implemented
+from .utils import load_readings, summarise
+
+# Define __all__ to explicitly control what `from sensorkit import *` exports
+__all__ = [
+    "Sensor",
+    "Thermocouple",
+    "load_readings",
+    "summarise",
+]

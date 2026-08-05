e5b718202c4f80c6ffa1013bd5e205c559c18b26
NB-CS50 <brownnana899@gmail.com>
2026-08-05 15:11:47 +0000

    Add files via upload

---
+++ sensorkit_project/sensorkit/stats.py
@@ -0,0 +1,28 @@
+"""
+Module: sensorkit/stats.py
+Contributor: Nana Amoabeng Brown
+Student ID: 95072029
+Date: 05/08/26
+Role: Simple summary statistics for a list of numeric readings.
+
+Complete the TODOs below.
+"""
+
+
+def mean(values):
+    if values != "":
+        return (sum(values)/len(values))
+    else:
+        raise ValueError("mean() needs values")
+
+
+def minimum(values):
+    return min(values)
+
+
+def maximum(values):
+    return max(values)
+
+
+def spread(values):
+    return (maximum(values) - minimum(values))

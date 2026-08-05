9c4270dfcf86e69a7e68cc3f239b98304aa86871
hvd4x <hudasuleman12@gmail.com>
2026-08-05 17:28:33 +0000

    Update __init__.py

--- sensorkit_project/sensorkit/__init__.py
+++ sensorkit_project/sensorkit/__init__.py
@@ -10,12 +10,8 @@ It exposes the public API so a user can simply write:
 If you add a StrainGauge class in sensors.py, add it to the import below too.
 """
 from .base import Sensor
-from .sensors import Thermocouple 
-from .utils import load_readings, summarise
+from .sensors import Thermocouple, PressureGauge, StrainGauge
+from .dataio import load_readings
+from .report import summarise
 
-__all__ = [
-    "Sensor",
-    "Thermocouple",
-    "load_readings",
-    "summarise",
-]
+__all__ = ["Sensor", "Thermocouple","PressureGauge","StrainGauge","load_readings","summarise",]
